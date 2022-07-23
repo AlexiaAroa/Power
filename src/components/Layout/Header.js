023cbda050f145fe178ce40a1621f534a935ceed
Alexia Aroa <alexia.1509rojo@gmail.com>
2022-07-23 19:51:55 -0300

    App y Header

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <hi>Power And Potion</hi>
+        <br></br>
+        <nav>
+          <ul>
+            <li>
+              <a href="/">Página principal</a>
+            </li>
+            <li>
+              <a href="/">Productos</a>
+            </li>
+            <li>
+              <a href="/">Sobre nosotros</a>
+            </li>
+            <li>
+              <a href="/">Contacto</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default Header;
